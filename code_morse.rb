bce7b3d2a2bcae213b65eac65c2ba1c76e199806
cvalencia1991 <cesar4a6z@gmail.com>
2022-12-14 18:16:36 -0500

    Fix linters Errors

--- code_morse.rb
+++ code_morse.rb
@@ -1,71 +1,41 @@
-
-
-
 def decoder_char(code_morse)
-  letteMorse = {
-    'a'=>'.-',
-    'b'=>'-...',
-    'c'=>'-.-.',
-    'e'=>'.',
-    'f'=>'..-.',
-    'g'=>'--.',
-    'h'=>'....',
-    'i'=>'..',
-    'j'=>'.---',
-    'k'=>'-.-',
-    'l'=>'.-..',
-    'm'=>'--',
-    'n'=>'-.',
-    'o'=>'---',
-    'p'=>'.--.',
-    'q'=>'--.-',
-    'r'=>'.-.',
-    's'=>'...',
-    't'=>'-',
-    'u'=>'..-',
-    'v'=>'...-',
-    'w'=>'.--',
-    'x'=>'-..-',
-    'y'=>'-.--',
-    'z'=>'--..',
-    '1'=>'.---',
-    '2'=>'..---',
-    '3'=>'...--',
-    '4'=>'....-',
-    '5'=>'.....',
-    '6'=>'-....',
-    '7'=>'--...',
-    '8'=>'---..',
-    '9'=>'----.',
-    '0'=>'-----'
+  lette_morse = {
+    'a' => '.-', 'b' => '-...', 'c' => '-.-.',
+    'e' => '.', 'f' => '..-.', 'g' => '--.',
+    'h' => '....', 'i' => '..', 'j' => '.---',
+    'k' => '-.-', 'l' => '.-..', 'm' => '--',
+    'n' => '-.', 'o' => '---', 'p' => '.--.',
+    'q' => '--.-', 'r' => '.-.', 's' => '...',
+    't' => '-', 'u' => '..-', 'v' => '...-',
+    'w' => '.--', 'x' => '-..-', 'y' => '-.--',
+    'z' => '--..', '1' => '.---', '2' => '..---',
+    '3' => '...--', '4' => '....-', '5' => '.....',
+    '6' => '-....', '7' => '--...', '8' => '---..',
+    '9' => '----.', '0' => '-----'
   }
-
-  letteMorse.each do |key, value|
-   if(value==code_morse)
-    return "#{key}".upcase
-    end
+  lette_morse.each do |key, value|
+    return key.to_s.upcase if value == code_morse
   end
 end
 
-#testing decoder_char
+# testing decoder_char
 # decoder_char('-.-.') #C
 # decoder_char('.')    #E
 # decoder_char('...')  #S
 # decoder_char('.-')   #A
 # decoder_char('.-.')  #R
 
-#Add method decode Word
+# Add method decode Word
 
 def docode_word(word_code)
   array = word_code.split(' ')
-  text = " "
+  text = ' '
   array.each do |letter|
     text += decoder_char(letter)
   end
-  return text
+  text
 end
 
+# docode_word("-- -.--") #MY
 
-docode_word("-- -.--")
-
-#Finish Function Decode Word
+# Finish Function Decode Word
